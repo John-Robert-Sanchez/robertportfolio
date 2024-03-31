73b85cfa08ee95b8b56a6f97a5453fea3dbd6844
John-Robert-Sanchez <165580604+John-Robert-Sanchez@users.noreply.github.com>
2024-04-01 01:57:24 +0800

    Portfolio robert

---
+++ About.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container-fluid"> <strong>
+      <p class="blue-text">I am a Bachelor of Science in Information Technology student. 
+       <br> I'm not just navigating coursework, I'm also facing life's challenges head-on with resilience and determination. <br>
+       persevere, driven by a strong individual spirit to succeed in both my personal and academic pursuits.</p>
+        </strong>
+    </div>
+  </template>
+  
+  <style scoped>
+  .container-fluid{
+    background-image: url('@/assets/robertback.png'); 
+    background-size: cover; 
+    background-repeat: no-repeat; 
+    background-position: center; 
+    padding: 200px;
+    border-radius: 10px;
+    color: #333; 
+    background-color: rgba(255, 255, 255, 0.7);
+    display: flex; /* Use flexbox */
+    justify-content: center; 
+    align-items: center; 
+    margin-top: 20px; 
+  }
+  
+  .blue-text {
+    color: #fff; 
+  }
+  </style>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
---
+++ Contact.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="contact-container">
+      <h4>Personal Information</h4>
+      <div class="contact-info">
+        <div class="contact-column">
+          <p><strong>Name:</strong> Robert Sanchez</p>
+          <p><strong>Gender:</strong> Male</p>
+          <p><strong>Birthdate:</strong> October 9 2000</p>
+          <p><strong>Age:</strong> 23</p>
+        </div>
+        <div class="contact-column">
+          <p><strong>Address:</strong> Babang Cordova Cebu</p>
+          <p><strong>School: </strong> Cordova Public College</p>
+          <p><strong>Course: </strong> Bachelor of Science and Information Technology</p>
+          <p><strong>Email Address:</strong> Johnrobertsanchez81@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .contact-container {
+    margin: 10%; 
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start; 
+    text-align: left; 
+    background-image: url('@/assets/robertback.png'); 
+    background-size: cover; 
+    background-repeat: no-repeat; 
+    padding: 200px; 
+    border-radius: 10px; 
+    color: #fff; 
+  }
+  
+  .contact-info {
+    display: flex;
+    flex-direction: column; 
+    gap: 20px; 
+  }
+  
+  .contact-column {
+    flex: 1;
+  }
+  </style>
+  
+  
+  
+  
+  
